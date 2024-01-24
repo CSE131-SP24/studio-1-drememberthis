b579eb5aae9a6d4ec377cd9890d8d1e7027319a2
anthe <anthe@DESKTOP-BDCFVLH.dhcp.wustl.edu>
2024-01-24 14:17:56 -0600

    Studio 1

--- src/studio1/HiFour.java
+++ src/studio1/HiFour.java
@@ -8,18 +8,27 @@ import java.util.Scanner;
  */
 public class HiFour {
 	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		System.out.print("Enter value for arg 0: ");
-		String s0 = in.nextLine();
-		System.out.print("Enter value for arg 1: ");
-		String s1 = in.nextLine();
-		System.out.print("Enter value for arg 2: ");
-		String s2 = in.nextLine();
-		System.out.print("Enter value for arg 3: ");
-		String s3 = in.nextLine();
+		//Scanner in = new Scanner(System.in);
+		//System.out.print("Bill ");
+		//String s0 = in.nextLine();
+		//System.out.print("Pat ");
+		//String s1 = in.nextLine();
+		//System.out.print("Mark ");
+		//String s2 = in.nextLine();
+		//System.out.print("Josh ");
+		//String s3 = in.nextLine();
 		//
 		// Say hello to the names in s0 through s3.
 		//
-
+		Scanner in = new Scanner(System.in);
+		String s0 = in.nextLine();
+		
+		String s1 = in.nextLine();
+		
+		String s2 = in.nextLine();
+		
+		String s3 = in.nextLine();
+		System.out.print("Greetings, " + s0 + ", " + s1 + ", " + s2 + ", and " + s3);
+		
 	}
 }
