b0dd3e5348e6318b3f42ae58c41765dd66c733e7
anthe <anthe@DESKTOP-BDCFVLH.dhcp.wustl.edu>
2024-01-24 14:18:15 -0600

    Studio 1 (part 2)

--- src/studio1/LeapYear.java
+++ src/studio1/LeapYear.java
@@ -1,10 +1,15 @@
 package studio1;
 
+import java.util.Scanner;
+
 public class LeapYear {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner in = new Scanner(System.in);
+		int year = in.nextInt();
+		
+		boolean woah = year/4 && year/100;
 	}
 
 }
